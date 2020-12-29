215825a8ea7c84720a3ed1792591fe9675a0de49
surajv3 <surajv3@yahoo.com>
2020-12-28 19:00:30 -0800

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,24 @@
+class Ground{
+constructor(x,y,width,height){
+var Options={
+    isStatic: true
+}
+
+this.body = Bodies.rectangle(x,y,width,height, Options)
+this.width = width
+this.height = height
+World.add(world, this.body)
+
+
+}
+display(){
+var pos = this.body.position;
+rectMode(CENTER)
+fill(255);
+rect(pos.x, pos.y, this.width, this.height)
+
+
+}
+
+
+} 
